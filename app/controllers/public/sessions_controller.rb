25f8650744eba9440835357a8dc296da19aaaf5a
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-09-29 06:53:45 +0000

    [ Add public] 9/29

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Admin::SessionsController < Devise::SessionsController
-    layout 'admin'
+class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  
   # GET /resource/sign_in
   # def new
   #   super
@@ -19,15 +19,7 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-   protected
-   
-   def after_sign_in_path_for(resource)
-     admin_dashboards_path
-   end
-   
-   def after_sign_out_path_for(resource_or_scope)
-     new_admin_session_path
-   end
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,23 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
-  
+
+
+namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+scope module: :public do
 devise_for :users
 root to: "homes#top"
 get '/homes/about' => 'homes#about', as:'about'
 resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :favorite, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
-
 end
-
-
 resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
 end
